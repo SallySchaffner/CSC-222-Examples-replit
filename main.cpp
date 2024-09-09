3fd85b42f2cee40e0cdb3d978dfaeee61b6e91a6
Sally Schaffner <151073463+SallySchaffner@users.noreply.github.com>
2024-09-09 18:47:37 +0000

    Added program code.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
-// This is the starting point for the Examples project
+// Percentage Discount
+//
+
 #include <iostream>
 using namespace std;
 
-int main() { cout << "CSC 222 Example Project\n"; }
+int main()
+{
+  double regularPrice, percentDiscount, discountAmount, reducedPrice;
+
+  regularPrice = 59.95;
+  percentDiscount = 0.2;
+
+  discountAmount = regularPrice * percentDiscount;
+  reducedPrice = regularPrice - discountAmount;
+
+  cout << "The discount amount is $" << discountAmount << endl;
+  cout << "The discounted price is $" << reducedPrice;
+
+  return 0;
+}
