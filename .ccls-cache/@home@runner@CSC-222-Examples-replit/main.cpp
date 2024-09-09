1e936a9879227d034203e3d9134811801dd61645
Sally Schaffner <151073463+SallySchaffner@users.noreply.github.com>
2024-09-09 18:30:11 +0000

    Initial version of Calculating Percentages

--- .ccls-cache/@home@runner@CSC-222-Examples-replit/main.cpp
+++ .ccls-cache/@home@runner@CSC-222-Examples-replit/main.cpp
@@ -1,5 +1,22 @@
-// This is the starting point for the Examples project
+// Calculating Percentages
+//
+
 #include <iostream>
 using namespace std;
 
-int main() { cout << "CSC 222 Example Project\n"; }
+int main()
+{
+    double salary = 6000;
+    double percent1 = 5, percent2 = 7, percent3 = 10;
+    double contribution1, contribution2, contribution3;
+
+    contribution1 = salary * percent1 / 100;
+    contribution2 = salary * percent2 / 100;
+    contribution3 = salary * percent3 / 100;
+
+    cout << "Amount of contribution at " << percent1 << "% is $" << contribution1 << endl;
+    cout << "Amount of contribution at " << percent2 << "% is $" << contribution2 << endl;
+    cout << "Amount of contribution at " << percent3 << "% is $" << contribution3 << endl;
+
+    return 0;
+}
